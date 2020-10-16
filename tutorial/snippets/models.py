149d1762d1d5cd5e27b306fedb393c8608f3e5f8
willwan92 <wilson3cy@163.com>
2020-10-16 15:48:22 +0800

    创建model和serializer类

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from pygments.lexers import get_all_lexers
+from pygments.styles import get_all_styles
 
-# Create your models here.
+# 该文件中定义变量和类可以再序列化类中导入使用
+# 定义需要用的变量
+LEXERS = [item for item in get_all_lexers() if item[1]]
+LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
+STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
+
+# 创建 model
+class Snippet(models.Model):
+    """
+    定义字段
+    """
+    created = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    styles = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+
+    class Meta:
+        ordering = ('created',)
