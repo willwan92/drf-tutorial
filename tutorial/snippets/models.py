fb52e06567748895a1617e25936c798580da7b4b
willwan92 <wilson3cy@163.com>
2020-10-16 16:12:19 +0800

    修复model字段名拼写错误

--- tutorial/snippets/models.py
+++ tutorial/snippets/models.py
@@ -18,7 +18,7 @@ class Snippet(models.Model):
     code = models.TextField()
     linenos = models.BooleanField(default=False)
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
-    styles = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
     class Meta:
         ordering = ('created',)
